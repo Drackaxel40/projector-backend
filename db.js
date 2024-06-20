86e0434eaf3b79374c3bbf19338101f6fd1fa0ab
Axel <>
2024-06-20 22:39:51 +0200

    Remade the DbQuery for better performance

--- db.js
+++ db.js
@@ -2,44 +2,49 @@ import mysql from 'mysql2/promise';
 
 // Database class
 class DB {
-    // private to avoid misuse in client code
-    #connection;
-    #connectionOptions;
+  // private to avoid misuse in client code
+  #connection;
+  #connectionOptions;
 
-    constructor(connectionOptionsArg) {
-        this.#connectionOptions = connectionOptionsArg;
-    }
+  constructor(connectionOptionsArg) {
+    this.#connectionOptions = connectionOptionsArg;
+  }
 
-    // Connect to the database
-    async connect() {
-        this.#connection = await mysql.createConnection(this.#connectionOptions);
-        return this;
+  // Connect to the database
+  async connect() {
+    if (!this.#connection) {
+      this.#connection = await mysql.createConnection(this.#connectionOptions);
     }
+    return this;
+  }
 
-    // Disconnect from the database
-    disconnect() {
-        this.#connection.destroy();
+  // Disconnect from the database
+  async disconnect() {
+    if (this.#connection) {
+      await this.#connection.end();
+      this.#connection = null;
     }
+  }
 
-    // Query the database
-    query(sql, value) {
-        return this.#connection.query(sql, value);
-    }
-};
+  // Query the database
+  async query(sql, value) {
+    await this.connect();
+    const result = await this.#connection.query(sql, value);
+    return result;
+  }
+}
 
 // Get the connection options from the environment
 const db = new DB({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 // Export the database query function
 export const dbQuery = async (sql, value) => {
-  await db.connect();
   const result = await db.query(sql, value);
-  db.disconnect();
   return result;
-}
+};
